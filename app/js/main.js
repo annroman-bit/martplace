4a1d45503ca6ccf6d5fd6f7367c4f8b656dd84b5
annroman-bit <annroman.bit@gmail.com>
2020-02-13 21:47:59 +0200

    added slick in section weekly and added section newest

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,25 @@
-$(function(){
+$(function () {
 
-    $('.categories').on('click', function(){
-        $('.categories__list > ul').slideToggle();
-      });
+
+  const ul = document.querySelector('ul.categories__list');
+  ul.addEventListener('click', evt => {
+    const ulSibling = evt.target.nextElementSibling;
+    slideToggle.slideToggle(ulSibling, 500);
+  });
+
+
+  $(".rate-star").rateYo({
+    rating: 5,
+    starWidth: "12px",
+    readOnly: true
+  });
+
+  $('.weekly__slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: false,
+    arrows: true
+  });
 
 });
